f50d39bcabbd040c23954e3d3a8ef5af69db6848
AmalkrishnaAS <amalkrishna02016@gmail.com>
2021-08-25 17:57:51 +0530

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    let API_KEY="" //enter the API key here
+    $("#form").submit(function(event) {
+        event.preventDefault();
+        let search=$("#search").val();
+        let video=''
+        videoSearch(API_KEY,search,5)
+    })
+    function videoSearch(key,search,count){
+        $("#videos").empty()
+        if(search.trim().length!=0){
+        $.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key="+key+"&type=video&part=snippet&maxResults="+count+"&q="+search,function(data){console.log(data)
+    data.items.forEach(function(item){
+        video=`<iframe width="560" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+        $("#videos").append(video);
+    }
+    )})
+    }
+else
+alert("please input s valid keyword")}
+
+})
